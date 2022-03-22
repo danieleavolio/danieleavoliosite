f2199c6ca93966ed96c03dd2b5c0aa070a3dbe76
lovaion <67899461+lovaion@users.noreply.github.com>
2022-03-22 21:33:02 +0100

    sitemap

--- src/routes/sitemap.xml.js
+++ src/routes/sitemap.xml.js
@@ -1,18 +1,85 @@
-import fs from 'fs';
+import GhostContentAPI, { PostsOrPages } from '@tryghost/content-api'
+import { API_URL, CONTENT_API_KEY } from '$lib/util/env'
 
 export async function get() {
-    try {
-        const data = fs.readFileSync('sitemap.xml', 'utf8');
+    const staticPages = Object.keys(
+        import.meta.glob('/src/routes/**/!(_)*.svelte')).filter(page => {
+        const filters: Array < string > = [
+            'slug]',
+            '_',
+            'private',
+            '/src/routes/index.svelte',
+        ]
 
-        return {
-            headers: {
-                'Content-Type': 'application/xml'
-            },
-            body: data
-        };
-    } catch (e) {
-        return {
-            body: 'Error'
-        };
+        return !filters.find(filter => page.includes(filter))
+    }).map(page => {
+        return page.replace('/src/routes', 'https://example.com').replace('/index.svelte', '').replace('.svelte', '')
+    })
+
+    const api = new GhostContentAPI({
+        url: `${API_URL}`,
+        key: `${CONTENT_API_KEY}`,
+        version: 'v3',
+    })
+
+    const posts = await api.posts.browse({
+        limit: 'all',
+        fields: ['slug', 'updated_at'],
+    })
+
+    const pages = await api.pages.browse({
+        limit: 'all',
+        fields: ['slug', 'updated_at'],
+    })
+
+    const body = render(pages, staticPages, posts)
+
+    const headers = {
+        'Cache-Control': `max-age=0, s-max-age=${600}`,
+        'Content-Type': 'application/xml',
+    }
+
+    return {
+        body,
+        headers,
     }
 }
+
+const render = (pages: PostsOrPages, staticPages: Array < string > , posts: PostsOrPages) => `<?xml version="1.0" encoding="UTF-8" ?>
+<urlset
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
+  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
+  xmlns:video="http://www.google.com/schemas/sitemap-video/1.1"
+  xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"
+  xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0"
+  xmlns:pagemap="http://www.google.com/schemas/sitemap-pagemap/1.0"
+  xmlns:xhtml="http://www.w3.org/1999/xhtml"
+>
+
+${pages.map(page => `
+  <url>
+    <loc>https://example.com/${page.slug !== 'homepage' ? page.slug : ''}</loc>
+    <lastmod>${new Date(page.updated_at).toISOString()}</lastmod>
+    <changefreq>weekly</changefreq>
+  </url>
+`)}
+
+${staticPages.map(staticPage => `
+  <url>
+    <loc>${staticPage}</loc>
+    <lastmod>${`${process.env.VITE_BUILD_TIME}`}</lastmod>
+    <changefreq>monthly</changefreq>
+  </url>
+`)}
+
+  ${posts.map((post) => `
+    <url>
+      <loc>https://example.com/posts/${post.slug}</loc>
+      <lastmod>${new Date(post.updated_at).toISOString()}</lastmod>
+      <changefreq>daily</changefreq>
+    </url>
+  `)}
+</urlset>
+`
